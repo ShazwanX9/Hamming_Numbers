10dfd6325e07d5ea8185dee2e16ef027823d5e58
Shazwan <76102465+ShazwanX9@users.noreply.github.com>
2021-09-01 00:06:48 +0800

    Add files via upload

---
+++ ReadMe.md
@@ -0,0 +1,18 @@
+# Hamming Number
+
+___
+In computer science, regular numbers are often called Hamming numbers, after Richard Hamming, 
+who proposed the problem of finding computer algorithms for generating these numbers in ascending order.
+___
+
+    In number theory, these numbers are called 5-smooth, 
+    because they can be characterized as having only 2, 3, or 5 as prime factors. 
+
+
+
+## DISCLAIMER!
+    no guarantee of completeness, accuracy, timeliness or 
+    of the results obtained from the use of this information
+
+### For more information:
+	Visit [Wikipedia](https://en.wikipedia.org/wiki/Regular_number)
---
+++ hamming_numbers.py
@@ -0,0 +1,113 @@
+"""
+In computer science, regular numbers are often called Hamming numbers, after Richard Hamming, 
+who proposed the problem of finding computer algorithms for generating these numbers in ascending order.
+
+In number theory, these numbers are called 5-smooth, 
+because they can be characterized as having only 2, 3, or 5 as prime factors. 
+"""
+
+class Hamming:
+    __doc__ = __doc__
+
+    @classmethod
+    def get_nth(self, n):
+        """
+        :param n:   int     nth position
+        :return:    int     nth Hamming Nummber
+        """
+        if n <= 0: raise ValueError("n should be more than 0 (positive integer)")
+
+        table = [1] * n
+        i, j, k = 0, 0, 0
+        ri, rj, rk = 2, 3, 5
+        
+        for ref in range(1, n):
+            table[ref] = min(ri, min(rj, rk))
+            if table[ref] == ri:
+                i += 1
+                ri = 2 * table[i]
+            if table[ref] == rj:
+                j+= 1
+                rj = 3 * table[j]
+            if table[ref] == rk:
+                k += 1
+                rk = 5 * table[k]
+        return table[-1]
+    
+    @classmethod
+    def is_hamming_number(self, n):
+        """
+        :param n:   int     positive integer to test
+        :return:    bool    the number is hamming number or not
+        """
+        if n <= 0: raise ValueError("n should be more than 0 (positive integer)")
+
+        ref = n
+        while ref>1:
+            if   ref%2==0:  ref//=2
+            elif ref%3==0:  ref//=3
+            elif ref%5==0:  ref//=5
+            else: return False
+        return True
+
+
+if __name__ == "__main__":
+    print("Testing for " + __file__)
+
+    ###############################################################################
+
+    RES = (
+        -1, 1, 2, 3, 4, 5, 6, 8, 9, 10, 
+        12, 15, 16, 18, 20, 24, 25, 27, 
+        30, 32, 36, 40, 45, 48, 50, 54
+    )
+
+    ###############################################################################
+
+    print("\nTesting get_nth: ")
+
+    for n in range(1, len(RES)):
+        check = Hamming.get_nth(n)
+        if n%5==1: print("\n\t", end='')
+        print(check,  "==", RES[n], check==RES[n], end = " : ")
+    print()
+
+    ###############################################################################
+
+    print("\nTesting is_hamming_number:\n")
+
+    #############
+
+    print("\tShould Return True")
+
+    for n in range(1, len(RES)):
+        if Hamming.is_hamming_number(RES[n]):
+            print("\t\t==>", True)
+            break
+    else:
+        print("\t\t==>", False)
+
+    #############
+
+    print("\tShould Return False")
+
+    for n in range(11, 20):
+        if n not in RES:
+            if Hamming.is_hamming_number(n):
+                print("\t\t==>", True)
+                break
+    else:
+        print("\t\t==>", False)
+
+    #############
+
+    print("\tShould Return True")
+
+    for i in range(20, 50):
+        if Hamming.is_hamming_number(Hamming.get_nth(n)): 
+            print("\t\t==>", True)
+            break
+    else:
+        print("\t\t==>", False)
+
+    ###############################################################################
